4cd9b04eb3e84a6bf0193d24199eb1321729a2b6
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2022-01-23 10:44:39 +0000

    Create emperor.h

---
+++ dwm/themes/emperor.h
@@ -0,0 +1,15 @@
+/* tagging */
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+/* fonts & size */
+static const char *fonts[]          = { "Iosevka:size=12" };
+static const char dmenufont[]       = "Iosevka:size=12";
+
+
+/* colors */
+static const char col_gray1[] = "#221f1c"; // deselected bg
+static const char col_gray2[] = "#878b94"; // deselected border
+static const char col_gray3[] = "#878b94"; // deselected text
+static const char col_gray4[] = "#9d5147"; // selected text
+static const char col_gray5[] = "#9d5147"; // selected border
+static const char col_cyan[] = "#221f1c"; // selected bg

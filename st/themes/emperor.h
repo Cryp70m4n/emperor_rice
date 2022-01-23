d2a3fbac905b2c4a65f11f262ff116e01bfd3506
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2022-01-23 10:47:08 +0000

    Create emperor.h

---
+++ st/themes/emperor.h
@@ -0,0 +1,24 @@
+static const char *colorname[] = {
+  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [1] = "#ea6962", /* red     */
+  [2] = "#a9b665", /* green   */
+  [3] = "#d8a657", /* yellow  */
+  [4] = "#7daea3", /* blue    */
+  [5] = "#d3869b", /* magenta */
+  [6] = "#89b482", /* cyan    */
+  [7] = "#d4be98", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#928374", /* black   */
+  [9]  = "#ef938e", /* red     */
+  [10] = "#bbc585", /* green   */
+  [11] = "#e1bb7e", /* yellow  */
+  [12] = "#9dc2ba", /* blue    */
+  [13] = "#e1acbb", /* magenta */
+  [14] = "#a7c7a2", /* cyan    */
+  [15] = "#e2d3ba", /* white   */
+};
+ unsigned int defaultfg = 15;
+ unsigned int defaultbg = 0;
+ unsigned int defaultcs = 15;
+ static unsigned int defaultrcs = 257;
